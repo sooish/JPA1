45f7896ced5646b349a7bc063d57c318d87baecf
Sue Kim <51253930+sooish@users.noreply.github.com>
2019-09-20 11:29:50 +0900

    Update JPA03CRUD.java

--- jpaproject1/src/main/java/running/JPA03CRUD.java
+++ jpaproject1/src/main/java/running/JPA03CRUD.java
@@ -9,52 +9,70 @@ import entity.Emp_copy;
 
 
 public class JPA03CRUD {
+	
+	public static void searchOne(int empno) {
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("encore");
+		EntityManager em = factory.createEntityManager();
+		EntityTransaction tx = em.getTransaction();
+		tx.begin();
 
-			public static void remove() {
-				 
-				EntityManagerFactory factory = Persistence.createEntityManagerFactory("encore");			 
-				EntityManager em = factory.createEntityManager();						 
-				EntityTransaction tx = em.getTransaction();
-				tx.begin();
-				
-			 
-				Emp_copy entity = em.find(Emp_copy.class, 8000);  
-				//entity.setName("MARIO");
-				em.remove(entity);
-			 
-				tx.commit();
-			 
-				em.close();
-				factory.close();
-			}
-			
-//			public static void update() {
-//				// factory 생성
-//				EntityManagerFactory factory = Persistence.createEntityManagerFactory("encore");
-//						
-//				// manager 생성
-//				EntityManager em = factory.createEntityManager();		
+		Emp_copy entity = em.find(Emp_copy.class, empno);
+		System.out.println(entity);
+		
+		tx.commit();
+
+		em.close();
+		factory.close();
+	}
+	
+//	public static void update() {
+//		// factory 생성
+//		EntityManagerFactory factory = Persistence.createEntityManagerFactory("encore");
 //				
-//				// tx 생성
+//		// manager 생성
+//		EntityManager em = factory.createEntityManager();		
+//		
+//		// tx 생성
+//		EntityTransaction tx = em.getTransaction();
+//		tx.begin();
+//		
+//		Emp_copy entity = em.find(Emp_copy.class, 8000);
+//
+//		entity.setEname("MARIO");
+//		
+//		// em.persist(entity);
+//		tx.commit();
+//		
+//		//자원반환
+//		em.close();
+//		factory.close();
+//	}
+
+//			public static void remove() {
+//				 
+//				EntityManagerFactory factory = Persistence.createEntityManagerFactory("encore");			 
+//				EntityManager em = factory.createEntityManager();						 
 //				EntityTransaction tx = em.getTransaction();
 //				tx.begin();
 //				
-//				Emp_copy entity = em.find(Emp_copy.class, 8000);
-//
-//				entity.setEname("MARIO");
-//				
-//				// em.persist(entity);
+//			 
+//				Emp_copy entity = em.find(Emp_copy.class, 8000);  
+//				//entity.setName("MARIO");
+//				em.remove(entity);
+//			 
 //				tx.commit();
-//				
-//				//자원반환
+//			 
 //				em.close();
 //				factory.close();
-//							}
+//			}
+//			
+
 
 			public static void main(String[] args) {
-//				update();
-				
-				remove();
+ 				searchOne(8000);
+//				update();			
+//				remove();
+ 				
 			}
 			
 		}
